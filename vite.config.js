dc682f8868f2f922d22ce10ce939c2c54911d337
Gregoire  Careme <122733828+GreggCarem@users.noreply.github.com>
2025-05-24 20:04:52 +0200

    Issues with sass

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portafolio-gregg/",
+  base: "/Portafolio-Gregoire-Careme",
   plugins: [react()],
 });
